0b7b943f112224ea60f6eaaad13e0d33e79aaac6
Nick Spera <nickaspera@gmail.com>
2019-08-02 02:54:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,10 +28,28 @@ function ternaryTeenager(age) {
  /* this code below is broken */
 function switchAge(age) {
   switch(age){
-    case(age >=13 && age <=19):
+    case(13):
      return "You are a teenager";
      break;
-    default:
-     return "You have an age";
+   case(14):
+    return "You are a teenager";
+    break;
+   case(15):
+    return "You are a teenager";
+    break;
+   case(16):
+    return "You are a teenager";
+    break;
+  case(17):
+    return "You are a teenager";
+    break;
+  case(18):
+    return "You are a teenager";
+    break;
+  case(19):
+    return "You are a teenager";
+    break;
+  default:
+   return "You have an age";
   }
 }
