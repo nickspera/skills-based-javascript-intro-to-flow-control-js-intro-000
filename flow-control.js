c96633b8ddb2f494aa12ada4bbaf50ec5d550c04
Nick Spera <nickaspera@gmail.com>
2019-08-02 02:44:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,8 +31,6 @@ function switchAge(age) {
     case(age >=13 && age <=19):
      return "You are a teenager";
      break;
-     case "You have an age";
-     break;
     default:
      return "You have an age";
   }
